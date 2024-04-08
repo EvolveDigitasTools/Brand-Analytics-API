a156ff133a79197cea0cbbb567e110863fe0d2f5
jatin <adriano23012001@gmail.com>
2024-04-08 12:33:46 +0530

    Checking all the env values are correct or not

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import productRouter from './product.route';
 const router = Router();
 
 router.get("/", async (req, res) => {
-	res.status(200).send(`Api is working - ${process.env.DB_NAME}`);
+	res.status(200).send(`Api is working - ${process.env.DB_NAME} - ${process.env.DB_USER} - ${process.env.DB_PASSWORD} - ${process.env.DB_HOST}`);
 });
 
 // router.use("/bulk-analytics", bulkAnalyticsRouter);
--- src/routes/shopify.route.ts
+++ src/routes/shopify.route.ts
@@ -4,5 +4,8 @@ import { newOrderShopify } from "../controllers/shopify.controller";
 const router = Router();
 
 router.post('/new-order', newOrderShopify)
+router.get("/", async (req, res) => {
+	res.status(200).send(`Shopify Api is working`);
+});
 
 export default router;
